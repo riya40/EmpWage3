0b8be88e65dd035476e3437140755a07a805f36d
riya40 <priyanunna20@gmail.com>
2021-12-16 21:39:00 +0530

    Added:Employee Presence

--- src/com/bl/ewc/EmployeeWageComputation.java
+++ src/com/bl/ewc/EmployeeWageComputation.java
@@ -2,9 +2,20 @@ package com.bl.ewc;
 
 public class EmployeeWageComputation {
     public static void main(String args[]){
-        
+
         //Displaying The Welcome Message
         System.out.println("Welcome To Employee Wage Computation");
+
+        //checking the Employee Presence
+        int rand = (int) Math.floor(Math.random() * 10) % 3;
+
+        if(rand==0){
+            System.out.println("Employee absent");
+        }
+        else {
+            System.out.println("employee present");
+            
+        }
 
     }
 }
